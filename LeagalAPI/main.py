5fb84d7fa78b98221d275e3c099f8788003ea049
Qubits <rakshanvenkat8@gmail.com>
2026-09-24 23:11:31 +0530

    added working gemini model 3.1 due to rate limit but works

--- LeagalAPI/main.py
+++ LeagalAPI/main.py
@@ -1,7 +1,20 @@
+import os
 from fastapi import FastAPI
+from pydantic import BaseModel
+import google.generativeai as genai
 
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+class RequestBody(BaseModel):
+    doc_type: str
+    detials: str
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "LegalEase Backend is running!"}
+@app.post("/generate")
+def gen_doc(request: RequestBody):
+    try:
+        model = genai.GenerativeModel("gemini-3.1-flash-lite")
+        promt = f" Act as an expert legal assistant. Generate a {request.doc_type} document with the following details: {request.detials}. Make it professional, clear, and structured"
+        response = model.generate_content(promt)
+        return {"document": response.text}
+    except Exception as e:
+        return {"error": str(e)}
+    
